c1290a04e1ebc6874fe8bdcbf6816c149e642369
DarlanHildebrando <darlan_m_silva@estudante.sesisenai.org.br>
2025-05-14 12:15:04 -0300

    povoando o repositório com React, template Vite

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+function App() {
+
+  return (
+    <div style={{textAlign: "center", color: "white", fontFamily: "sans-serif"}}>
+     <h1>Em produção!!!!</h1> 
+    </div>
+  );
+}
+
+export default App;
